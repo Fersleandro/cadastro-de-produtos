b0a02bc76787de2d8ed193400eded66c68ffd13c
Fernando Silva <118748241+Fersleandro@users.noreply.github.com>
2023-03-25 00:12:43 -0300

    Atualização criando index.html

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let numeros = [2, 4, 6, 8, 10]
 //Percorrendo a lista
 for(let i = 0; i < numeros.length; i++)
 {
-  if(numeros[i] % 2 === 0
+  if(numeros[i] % 2 === 0)
   {
     console.log(numero[i] + " é um número par")
   }
@@ -12,3 +12,4 @@ for(let i = 0; i < numeros.length; i++)
   {
     console.log(numero[i] + " é um número impar")
   }
+}
